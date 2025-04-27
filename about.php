da744a93fac6066fdd6a1a9694cc12a502acf1f2
arya5-lab <aryaramsurn55@gmail.com>
2025-04-27 14:34:46 +0200

    Update about.php

--- about.php
+++ about.php
@@ -43,7 +43,6 @@ $conn->close();
           <li><a href="properties.php">Properties</a></li>
           <li><a href="contact.php">Contact</a></li>
           <?php if (isset($_SESSION['user_id'])): ?>
-            <li><a href="profile.php">Profile</a></li>
             <li><a href="logout.php">Logout</a></li>
           <?php else: ?>
             <li><a href="login.php">Login</a></li>
